bb630c3900f8c44d26937d8dbc6ee53266533953
Harto <anine.harto@accenture.com>
2020-02-05 11:09:37 +0100

    Modified black_box_check

--- quantum_functions.py
+++ quantum_functions.py
@@ -3,18 +3,30 @@ import pandas as pd
 
 
 def uint4(int_or_string):
-    # Simulate 4-bit unsigned integer by masking 4 bits of numpy.unit8
+    """
+    Simulate 4-bit unsigned integer by masking 4 bits of numpy.unit8
+
+    :param int_or_string:
+    :return:
+    """
     unit8_type = np.uint8(int_or_string)
     mask = np.uint8(2 ** 4 - 1)
     return unit8_type & mask
 
 
-def black_box_check(circuit, key, q):
-    # Black box oracle function to encode a winner entry in a dataset
-    # Inverts the phase of 'q' if 'key' and 'q' are matching, otherwise the 'q' remains unchanged.
-    # Note: the implementation of the oracle here is only for simulation purposes, and its details may not have
-    # any practical relevance. In practice, the oracle is generated systematically for each problem ecncoding
-    # a function (or dataset) that cannot be inverted (or efficiently searched).
+def black_box_check(circuit, key, q, problem_type='easy'):
+    """
+    Black box oracle function to encode a winner entry in a dataset
+    Inverts the phase of 'q' if 'key' and 'q' are matching, otherwise the 'q' remains unchanged.
+    Note: the implementation of the oracle here is only for simulation purposes, and its details may not have
+    any practical relevance. In practice, the oracle is generated systematically for each problem encoding
+    a function (or dataset) that cannot be inverted (or efficiently searched).
+
+    :param circuit: the quantum circuit
+    :param key: which element should be located ('apple', 'banana' etc.)
+    :param q: the quantum register
+    :param problem_type: default 'easy' to search in fruits.csv, and 'complex' to search in fruits_extended.csv
+    """
 
     data_frame = pd.read_csv("data/fruits.csv", sep=';')
     data_dict = data_frame.set_index('name').T.to_dict('records')[0]
@@ -36,7 +48,13 @@ def black_box_check(circuit, key, q):
 
 
 def reflection_about_average(circuit, q):
-    # Reflection about average for amplitude amplification
+    """
+    Reflection about average for amplitude amplification
+
+    :param circuit: the quantum circuit
+    :param q: the quantum register
+    """
+
     circuit.h(q)
     circuit.x(q)
     circuit.h(q[1])
@@ -44,3 +62,4 @@ def reflection_about_average(circuit, q):
     circuit.h(q[1])
     circuit.x(q)
     circuit.h(q)
+
